19b6cb9f3ea7b82bca49145b476597b9f64c2020
rob2301 <rob230191@gmail.com>
2022-09-30 12:37:11 +0200

    set pu place component

--- frontend/src/components/place.tsx
+++ frontend/src/components/place.tsx
@@ -1,7 +1,76 @@
 import { useEffect, useState } from 'react';
+const apiKey = process.env.REACT_APP_API_KEY;
+
+interface CurrentWeatherData {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  };
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grdn_level: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  coluds: {
+    all: number;
+  };
+  rain: {
+    '1h': number;
+    '3h': number;
+  };
+  snow: {
+    '1h': number;
+    '3h': number;
+  };
+  dt: number;
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
 
 export default function Place() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<CurrentWeatherData>();
 
-  return <div>place</div>;
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=48.10&lon=20.79&units=metric&appid=${apiKey}`
+    )
+      .then((res) => res.json())
+      .then((json) => setData(json))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <>
+      <div>Város: {data?.name}</div>
+      <div>Altuális hőmérséklet: {data?.main.temp} °C</div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </>
+  );
 }
